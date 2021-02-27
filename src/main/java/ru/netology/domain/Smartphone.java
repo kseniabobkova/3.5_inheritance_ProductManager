f8981fb42496b158f7b8ce73c84136ca4fc825eb
kseniabobkova <bobkova@healbe.com>
2021-02-27 20:40:57 +0300

    add java

---
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -0,0 +1,22 @@
+package ru.netology.domain;
+
+import java.util.Objects;
+
+public class Smartphone extends Product {
+    private String producer;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Smartphone shirt = (Smartphone) o;
+        return Objects.equals(producer, shirt.producer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), producer);
+    }
+}
---
+++ src/test/java/ru/netology/domain/SmartphoneTest.java
@@ -0,0 +1,35 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SmartphoneTest {
+    @Test
+    public void shouldHaveAllFieldsAndMethodFromSuperClass() {
+        Smartphone smartphone = new Smartphone();
+//    book.
+    }
+
+    @Test
+    public void shouldCastFromBaseClass() {
+        Product product = new Smartphone();
+        if (product instanceof Smartphone) {
+            Smartphone smartphone = (Smartphone) product;
+//      book.
+        }
+    }
+
+    @Test
+    public void shouldNotCastToDifferentClass() {
+        Product product = new Smartphone();
+        Smartphone smartphone = (Smartphone) product;
+    }
+
+    @Test
+    public void shouldUseOverridedMethod() {
+        Product product = new Smartphone();
+        // Вопрос к аудитории: чей метод вызовется?
+        product.toString();
+    }
+}
